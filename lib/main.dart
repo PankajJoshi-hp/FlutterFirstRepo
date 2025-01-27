de054513062cdc7b69c06d8439b35af61147eddf
Pankaj Joshi <Pankaj.Joshi.hp@gmail.com>
2025-01-27 16:02:45 +0530

    Expand class implemented

--- lib/main.dart
+++ lib/main.dart
@@ -23,34 +23,78 @@ class ChildApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.redAccent,
-        appBar: AppBar(
-          // backgroundColor: Colors.transparent,
-          backgroundColor: Colors.lightGreen,
-          title: const Text('This is First App'),
-        ),
-        // body: Container(
-        //   alignment: AlignmentDirectional.center,
-        //   child: Text(
-        //     'This is the body of my first app',
-        //     style: TextStyle(fontSize: 24),
-        //   ),
-        // ),
-        // body: Row(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     Text('Hey'),
-        //     Text('This is text inside row'),
-        //   ],
-        // ));
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text('Hey'),
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        // backgroundColor: Colors.transparent,
+        backgroundColor: Colors.lightGreen,
+        title: const Text('This is First App'),
+      ),
+      // body: Container(
+      //   alignment: AlignmentDirectional.center,
+      //   child: Text(
+      //     'This is the body of my first app',
+      //     style: TextStyle(fontSize: 24),
+      //   ),
+      // ),
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: <Widget>[
+      //     Text('Hey'),
+      //     Text('This is text inside row'),
+      //   ],
+      // ),
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: <Widget>[
+      //     Center(
+      //       child: Text('Hey'),
+      //     ),
+      //     Text('This is text inside row'),
+      //   ],
+      // ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            height: 100,
+            child: Center(
+              child: Text(
+                'First widget',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
             ),
-            Text('This is text inside row'),
-          ],
-        ));
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              // width: 200,
+              child: Center(
+                child: Text(
+                  'Second widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.orange,
+            height: 100,
+            child: Center(
+              child: Text(
+                'Third widget',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
